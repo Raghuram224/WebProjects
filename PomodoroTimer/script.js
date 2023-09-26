6b87ceefb60448ba26120aad7740a69aad357c56
raghuram <raghuraghu11ram@gmail.com>
2023-09-26 10:36:13 +0530

    pomodoro added

---
+++ PomodoroTimer/script.js
@@ -0,0 +1,17 @@
+const countdownTimeInSeconds = 3600;
+let remainingTime = countdownTimeInSeconds;
+
+function timer(){
+    const minutes=Math.floor(remainingTime/60);
+    const seconds=remainingTime%60;
+
+    console.log(`${minutes}:${seconds}`);
+    if(remainingTime===0){
+        console.log("times up")
+        clearInterval(TIMEINTERVEL);
+    }else{
+        remainingTime--;
+    }
+
+}
+const TIMEINTERVEL=setInterval(timer,1000);
