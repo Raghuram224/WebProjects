649ee25e647cee9dabc938d2ad94e02cd6b9fa4a
raghuram <raghuraghu11ram@gmail.com>
2023-09-27 23:36:08 +0530

    range buttons added

--- PomodoroTimer/script.js
+++ PomodoroTimer/script.js
@@ -2,6 +2,7 @@ const countdownTimeInSeconds = 60;
 let remainingTime = countdownTimeInSeconds;
 let timer;
 let timerStatus = false;
+let dark_mode=0;
 
 const  TIME=_('time');
 const MESSAGE=_('msg');
@@ -9,7 +10,35 @@ const MESSAGE=_('msg');
 function _(id){
     return document.getElementById(id);
 }
+function darkMode(){
+    if(dark_mode%2==0){
+        document.body.style.backgroundColor='black';
+        document.body.style.color='white';
+        dark_mode++;
+        document.body.style.transition="350ms ease-in-out";
+        
+    }
+    else{
+        document.body.style.backgroundColor='white';
+        document.body.style.color='black';
+        document.body.style.transition="350ms ease-in-out";
+        dark_mode++;
+        
+    }
+   
 
+   
+}
+function updateTimer(){
+    let range=_('time-input');
+    let displayRange=_('time-range');
+    displayRange.textContent=range.value;
+}
+function updateBreak(){
+    let range=_('break-input');
+    let displayRange=_('break-range');
+    displayRange.textContent=range.value;
+}
 function reset(){
     TIME.textContent="00:00";
     pause();
