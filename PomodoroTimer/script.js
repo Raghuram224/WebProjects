8f355d341d0a89ab8dd98eff9c8623efe41c79ce
raghuram <raghuraghu11ram@gmail.com>
2023-09-27 12:04:24 +0530

    background clip done

--- PomodoroTimer/script.js
+++ PomodoroTimer/script.js
@@ -1,18 +1,34 @@
-const countdownTimeInSeconds = 3600;
+const countdownTimeInSeconds = 5;
 let remainingTime = countdownTimeInSeconds;
 
-function timer(){
+
+
+function startTimer(){
     const minutes=Math.floor(remainingTime/60);
     const seconds=remainingTime%60;
 
     console.log(`${minutes}:${seconds}`);
     if(remainingTime===0){
-        console.log("times up")
-        clearInterval(TIMEINTERVEL);
-    }else{
+        
+        MESSAGE.textContent= "Time's Up";
+        TIME.textContent="00:00";
+        clearInterval(startTimer);
+        
+    }else{        
+        TIME.textContent=`${minutes}:${seconds}`;
         remainingTime--;
     }
 
 }
-const TIMEINTERVEL=setInterval(timer,1000);
+function _(id){
+    return document.getElementById(id);
+}
+// const TIMEINTERVEL=0;
+const  TIME=_('time');
+const MESSAGE=_('msg');
 
+
+function start(){
+    let timeStarts= setInterval(startTimer,1000);
+    // TIME.textContent=timeStarts;
+}
