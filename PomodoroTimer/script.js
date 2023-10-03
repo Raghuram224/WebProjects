faa7858fa179517f95fb14594e36af344f152857
raghuram <raghuraghu11ram@gmail.com>
2023-10-03 21:16:54 +0530

    timer updated in js

--- PomodoroTimer/script.js
+++ PomodoroTimer/script.js
@@ -1,16 +1,30 @@
-let countdownTimeInSeconds = updateTimer*10 ;
-console.log(countdownTimeInSeconds);
+
+
+// let countdownTimeInSeconds =parseInt(updateTimer().value)*60;
+let countdownTimeInSeconds =1*60;
+
+let breakCount=0;
 let remainingTime = countdownTimeInSeconds;
 let timer;
-let startCount=0;
 let timerStatus = false;
 let dark_mode=0;
+let timeControl=0;
 
 const  TIME=_('time');
 const MESSAGE=_('msg');
+const STATUS=_('status');
 
 function _(id){
     return document.getElementById(id);
+}
+
+function breakTimer(){
+    // let breakTime= updateBreak();
+    let breakTime=1*60;
+    
+    remainingTime=breakTime;
+    start();
+
 }
 function darkMode(){
     if(dark_mode%2==0){
@@ -35,23 +49,30 @@ function darkMode(){
 function updateTimer(){
     let range=_('time-input');
     let displayRange=_('time-range');
-    
     displayRange.textContent=range.value;
-    return parseInt(displayRange.value);
+    
+    countdownTimeInSeconds = parseInt(range.value) * 60;
+    remainingTime = countdownTimeInSeconds;
+   
 }
 function updateBreak(){
     let range=_('break-input');
     let displayRange=_('break-range');
-    displayRange.textContent=range.value;
+
+    displayRange.textContent=range.value+"min";
+    
+    let result= Math.ceil(parseInt(range.value)/parseInt(range.value));
+   
+    return result;
+
+    
 }
 function reset(){
-    TIME.textContent="00:00";
+    TIME.textContent=remainingTime;
     pause();
     remainingTime=countdownTimeInSeconds;
     timerStatus=false;
-     
-    remainingTime = countdownTimeInSeconds;
-    MESSAGE.textContent=" ";
+    
 }
 
 function pause(){
@@ -62,35 +83,60 @@ function pause(){
 }
 function startTimer(){
     let minutes=Math.floor(remainingTime/60);
-    let seconds=remainingTime%60;
-    console.log(remainingTime);
     
-    console.log(`${minutes}:${seconds}`);
+    let seconds=remainingTime%60;
+   
+    
+    // console.log(`0${minutes}:${seconds}`);
     if(remainingTime===0){
         
         MESSAGE.textContent= "Time's Up";
-        TIME.textContent="00:00";
-        clearInterval(startTimer);
+        TIME.textContent=remainingTime;
+        clearInterval(startTimer);     
+         
+        breakCount++;
+        timeControl++;
+        console.log("breakCount"+breakCount);
+
+            if(breakCount>7){
+                remainingTime=0;
+                pause();
+                reset();
+            }
+        
+            _("display").textContent=breakCount;
+            if(breakCount==7){
+               remainingTime=3*60;
+               setTimeout(breakTimer);
+               MESSAGE.textContent=" ";
+                STATUS.textContent = "Long Break!!";
+
+            }
+            else if(timeControl%2==0){
+                setTimeout(start);
+                remainingTime=countdownTimeInSeconds;
+                MESSAGE.textContent=" ";
+                STATUS.textContent = "Work";
+                
+            }  
+            else {
+                setTimeout(breakTimer);
+                MESSAGE.textContent=" ";
+                STATUS.textContent = "Break";
+            }            
+             
+        // }
         
     }else{        
-        TIME.textContent=`${minutes}:${seconds}`;
-        console.log(remainingTime);
+        TIME.textContent=`0${minutes}:${seconds}`;
+        // console.log(remainingTime);
         remainingTime-=1;
     }
 
 }
 
 function start(){
-   console.log("range"+_('time-range').value);
-    if(startCount%2==0){
-        
-        remainingTime = countdownTimeInSeconds;
-        MESSAGE.textContent=" ";
-        
-        console.log(startCount)
-    }
-  
-    startCount++;
+    
     if(!timerStatus){
       timer=setInterval(startTimer,1000);
     timerStatus = true;
